4487982d8228adca548c32ce16e4678428690294
Ed Henderson <ed@sharpertool.com>
2017-07-04 19:10:37 -0600

    Update Docker ports
    Incremented the docker ports by one so that this project won't conflict
    with the synopticone project when developing locally.

--- datasheet_ai/datasheet_ai/settings/dev.py
+++ datasheet_ai/datasheet_ai/settings/dev.py
@@ -27,3 +27,8 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = 8026
+EMAIL_HOST = 'localhost'
+

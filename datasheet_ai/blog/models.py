6f840b759cba40615fd97f3c4b9fb99964eabfe0
Ed Henderson <ed@sharpertool.com>
2017-07-11 19:40:49 -0600

    Add blog app, some hopepage updates.

--- datasheet_ai/blog/models.py
+++ datasheet_ai/blog/models.py
@@ -1,15 +1,15 @@
-from __future__ import absolute_import, unicode_literals
-
 from django.db import models
+
+# Create your models here.
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
-class HomePage(Page):
-    body = RichTextField(blank=True)
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        FieldPanel('intro', classname="full")
     ]

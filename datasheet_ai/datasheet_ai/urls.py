ce457d19a6111ce1c7486403d1aba2292ca15754
ed@sharpertool.com <ed@sharpertool.com>
2018-03-06 15:26:44 -0700

    Update to latest wagtail, and Django 2.0. Update migrations

--- datasheet_ai/datasheet_ai/urls.py
+++ datasheet_ai/datasheet_ai/urls.py
@@ -21,10 +21,10 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from search import views as search_views
+from wagtail.search import views as search_views
 
 urlpatterns = [
-    url(r'^django-admin/', include(admin.site.urls)),
+    url(r'^django-admin/', admin.site.urls),
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),

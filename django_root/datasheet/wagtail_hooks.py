bc3154fc652eea52506c01e305575d142e4c0f93
ed@sharpertool.com <ed@sharpertool.com>
2018-03-25 15:33:33 -0600

    Moved the edit page override, and inserted the hook instread

--- django_root/datasheet/wagtail_hooks.py
+++ django_root/datasheet/wagtail_hooks.py
@@ -48,3 +48,10 @@ def add_datasheet_summary(request, items):
 @hooks.register('product_code')
 def product_code_hook():
     pass
+
+@hooks.register('insert_editor_css')
+def editor_css():
+    return format_html(
+        '<link rel="stylesheet" href="{}"',
+        static('css/datasheetadmin.css')
+    )

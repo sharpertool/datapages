95a319495b8a3437a60b7682dfbdf2bdf6642541
jdltechworks <jo@bigzeta.com>
2018-04-17 10:38:18 +0800

    Removing console.log

--- django_root/datasheet/static/js/print.js
+++ django_root/datasheet/static/js/print.js
@@ -1,6 +1,6 @@
 (function(w, d) {
-    console.log(d);
-    const print_button = d.getElementById('print');
+
+    var print_button = d.getElementById('print');
 
     print_button.addEventListener('click', function(e) {
         w.print();

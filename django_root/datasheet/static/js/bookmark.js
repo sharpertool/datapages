e612b870734f038e1e89e6cbeaba546b7b9edb4a
jdltechworks <jo@bigzeta.com>
2018-06-29 16:21:01 +0800

    Adding sticky nav

--- django_root/datasheet/static/js/bookmark.js
+++ django_root/datasheet/static/js/bookmark.js
@@ -1,24 +1,37 @@
-(function($) {
-    //jump-to-section
+(function(d, target, $) {
+
+    const sidebar = d.querySelector(target)
+
+
     $("a[data-action='anchor']").click(function(e) {
         var target = $(this.getAttribute('href'));
         $('html, body').animate({
-            scrollTop: $(target).offset().top - 100
+            scrollTop: $(target).offset().top - 135
         }, 1000);
         $(this).parents('li').addClass('active').siblings('li').removeClass('active');
         $('.jump-section-wrapper').trigger('click');
         e.preventDefault();
-    });
+    })
 
-    //jump-to-section-dropdown
-    $('.sidebar').on('click', '.jump-section-wrapper', function(e) {
-        e.preventDefault();
-        var $this = $(this);
-        if ($this.parents('.sidebar').find('ul').is(':not(":animated")')) {
-            $this.toggleClass('open');
-            $this.parents('.sidebar').find('ul').slideToggle('500');
-        } else {
-            console.log('already animating');
+    const floatSideBar = (e) => {
+        const yCoordinate = window.pageYOffset
+        const yOffset = sidebar.offsetTop - 120
+
+        if(yOffset <= yCoordinate) {
+            sidebar.classList.add('scrolled')
+            return
         }
-    });
-})($);
+
+        sidebar.classList.remove('scrolled')
+
+        return
+
+    }
+
+    const scrollObserver = () => {
+        floatSideBar()
+    }
+
+    document.addEventListener('scroll', scrollObserver)
+
+})(document, '.sidebar', jQuery);

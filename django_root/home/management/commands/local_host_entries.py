78e0344b80f60c0c46f1f19f3dd6cc03f8e8e70d
ed@sharpertool.com <ed@sharpertool.com>
2018-06-30 15:28:09 -0600

    Add management script to update host entires for /etc/hosts and allowed hosts

---
+++ django_root/home/management/commands/local_host_entries.py
@@ -0,0 +1,45 @@
+from os.path import exists
+from shutil import copy
+import re
+from django.core.management.base import BaseCommand
+from wagtail.core.models import Site
+
+
+class Command(BaseCommand):
+    help = 'Generate output to be added to /etc/hosts file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--env')
+        parser.add_argument('--etcfile')
+
+    def handle(self, *args, **options):
+
+        allowed_hosts = list(
+            Site.objects.values_list('hostname', flat=True).all())
+
+        etc_hosts = [f"127.0.0.1 {x}" for x in allowed_hosts]
+
+        hosts_lines = "\n".join(etc_hosts)
+
+        etc_data = """
+        
+            # Datapages.local begin
+            {hosts_lines}
+            # Datapages.local end
+        """
+
+        envfile = options.get('env', None)
+        if envfile and exists(envfile):
+            copy(envfile, envfile + '.bak')
+            with open(envfile, 'r') as fp:
+                original = fp.readlines()
+
+            with open(envfile, 'w') as fp:
+                for line in original:
+                    if re.search(r'^DJANGO_ALLOWED_HOSTS', line):
+                        line = f"DJANGO_ALLOWED_HOSTS={','.join(allowed_hosts)}"
+                    fp.write(line)
+
+        hostsfile = options.get('etcfile', None)
+        if hostsfile:
+            print(etc_data)

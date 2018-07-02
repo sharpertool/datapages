ec54ffd7c5ec6a4b123ba1a566f4b279ccef18e2
ed@sharpertool.com <ed@sharpertool.com>
2018-07-02 15:20:18 -0600

    Add scrollspy
    Added scrollspy as a javascript function
    - Updated menus to include the list-group and list-group-item classes
    - Updated the SCSS to override the list-group-item styling which we don’t want
    - Updated bookmark.js to initialize scrollspy logic.
    
    ToDo: Re-initialize scrollspy if the window size changes, so we can use the mobile OR desktop target. This isn’t critical, since I’m going to assume that any mobile will be always mobile, and not transitioning between mobile and desktop.

--- django_root/datasheet/static/js/bookmark.js
+++ django_root/datasheet/static/js/bookmark.js
@@ -2,22 +2,34 @@
 
     var jump_items = $('.jump-menu li')
     var toggle = $('.jump-toggle')
+    
+    var calc_offset = function calc_offset() {
+        // Return full height, with padding
+        var nav_h = parseInt($('.navbar').css('height'))
+        
+        var mm = $('.menu-mobile')
+        var h1 = mm.is(':hidden') ?
+            0 : parseInt($('.menu-mobile').css('height'))
+            
+        var offset = nav_h + h1
+
+        return offset
+    }
 
     $("a[data-action='anchor']").on('click', function(e) {
         e.preventDefault()
         var self = $(this)
         var target = self.attr('href')
-        // Return full height, with padding
-        var nav_h = $('.navbar').css('height')
-        var h1 = $('.menu-mobile').css('height')
+
+        var offset = calc_offset()
+        
         if (target == '#bookmark_introduction') {
             $('html, body').animate({scrollTop: 0}, 1000)
         } else {
             $('html, body').animate({
-                scrollTop: $(target).offset().top - (114 + parseInt(h1))
+                scrollTop: $(target).offset().top - offset
             }, 1000)
         }
-        $(this).parent().addClass('active').siblings().removeClass('active')
         toggle.trigger('click')
     })
 
@@ -31,5 +43,27 @@
         self.toggleClass(openClass)
         mnu.toggleClass(openClass, self.hasClass(openClass))
     });
+    
+    function setup_scrollspy() {
+        var mm = $('.menu-mobile')
+        var target = mm.is(':hidden') ?
+            '#sidebar_jump_list' : '#mobile_jump_list'
+
+        var offset = calc_offset()
+
+        // Update Active links when body scrolled
+        $('body').scrollspy({
+            target: target,
+            offset: offset
+        })
+        console.log(`Setup scrollspy on ${target}`)
+    }
+    
+    // setup the scrollspy on the main or mobile menu
+    // If the window is re-sized, then reset it
+    setup_scrollspy()
+    $(window).resize(function() {
+        setup_scrollspy()
+    })
 
 })(jQuery, 'open')

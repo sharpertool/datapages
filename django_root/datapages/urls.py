33dfd8d2751c37bf32a25033fa1d301ce436fa78
Ed Henderson <ed@bigzeta.com>
2018-08-13 23:55:37 -0500

    Fix issue in urls with deprecated search view

--- django_root/datapages/urls.py
+++ django_root/datapages/urls.py
@@ -23,8 +23,6 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from wagtail.search import views as search_views
-
 from clara import urls as clara_urls
 
 from .api import api_router
@@ -34,8 +32,6 @@ urlpatterns = [
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
-    url(r'^search/$', search_views.search, name='search'),
 
     # Clara URL
     url(r'^clara/', include(clara_urls)),
